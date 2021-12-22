fafaaf4400f5e3ba670080673adc7ff6655e2105
iamtanuj18 <75843627+iamtanuj18@users.noreply.github.com>
2021-12-22 22:20:00 +0530

    Add files via upload

---
+++ Question.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ id, title, info }) => {
+  const [toggle, setToggle] = useState(true);
+
+  return (
+    <>
+      <article className="question">
+        <header key={id}>
+          <h4>{title}</h4>
+          <button className="btn" onClick={() => setToggle(!toggle)}>
+            {toggle ? <AiOutlinePlus /> : <AiOutlineMinus />}
+          </button>
+        </header>
+        <p>{toggle ? "" : info}</p>
+      </article>
+    </>
+  );
+};
+
+export default Question;
